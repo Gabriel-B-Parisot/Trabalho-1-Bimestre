d45307196a4a547293833f778986f089ee9ab6ea
Gabriel B. Parisot <160789738+Gabriel-B-Parisot@users.noreply.github.com>
2026-09-18 20:34:02 -0300

    Add files via upload

---
+++ atividade/src/main/java/com/mycompany/atividade/Atividade.java
@@ -0,0 +1,8 @@
+package com.mycompany.atividade;
+
+public class Atividade {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
---
+++ atividade/src/main/java/com/mycompany/atividade/Recepcionistas.java
@@ -0,0 +1,5 @@
+package com.mycompany.atividade;
+
+public class Recepcionistas {
+    
+}
